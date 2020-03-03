dab014d70a3d895436d2d916cd69a33772107eb6
Aby Kal <abyabrkal@gmail.com>
2020-03-03 14:01:16 +0400

    Bulma site design

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+      <section class="hero is-medium is-dark is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">Users</h1>
+            <h2 class="subtitle">Details</h2>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default Hero
---
+++ src/components/StackDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/stackdisplay.css'
+
+const StackDisplay = () => {
+    return (
+        <div className="stacked">
+            <div className="stacked-lev2">
+                <div className="stacked-lev3">
+                    {/* <Table /> */} 5
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StackDisplay
